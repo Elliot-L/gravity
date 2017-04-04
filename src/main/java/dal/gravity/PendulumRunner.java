d1e2b2e395bccf06b416bda997b6d4b154a955be
Elliot Layne <e_layne@sympatico.ca>
2017-04-04 12:52:19 -0300

    Running the program with dynamically changing grav constants

--- src/main/java/dal/gravity/PendulumRunner.java
+++ src/main/java/dal/gravity/PendulumRunner.java
@@ -22,23 +22,39 @@ public class PendulumRunner {
 	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
 	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, delta, earth);
 	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0, earth);
-	RegularPendulum rpCoarse = 
-	    new RegularPendulum (sLen, pMass, theta0, .1, earth);
+	//RegularPendulum rpCoarse = 
+	  //  new RegularPendulum (sLen, pMass, theta0, .1, earth);
 
 	// print out difference in displacement in 1 second intervals
 	// for 20 seconds
 	int iterations = (int) (1/delta);
-	System.out.println ("analytical vs. numerical displacement (fine, coarse)");
+	System.out.println ("analytical vs. numerical displacement with Earth gravitational field");
 	for (int second = 1; second <= 20; second++) {
 	    for (int i = 0; i < iterations; i++) rp.step ();
-	    for (int i = 0; i < 10; i++) rpCoarse.step (); 
+	   // for (int i = 0; i < 10; i++) rpCoarse.step (); 
 	    System.out.println ("t=" + second + "s: \t" + 
 				nf.format (Math.toDegrees (sp.getTheta (second))) 
 				+ "\t" +
-				nf.format (Math.toDegrees (rp.getLastTheta ()))
-				+ "\t" + 
-				nf.format (Math.toDegrees (rpCoarse.getLastTheta ())));
+				nf.format (Math.toDegrees (rp.getLastTheta ())));
+			//	+ "\t" + 
+			//	nf.format (Math.toDegrees (rpCoarse.getLastTheta ())));
 	}
-    }
-}
+	
+	//running things with saturn gravity
+	System.out.println ("analytical vs. numerical displacement with Saturn gravitational field");
+	sp.setGravityConstant(saturn);
+	rp.setGravityConstant(saturn);
+	for (int second = 1; second <= 20; second++) {
+	    for (int i = 0; i < iterations; i++) rp.step ();
+	   // for (int i = 0; i < 10; i++) rpCoarse.step (); 
+	    System.out.println ("t=" + second + "s: \t" + 
+				nf.format (Math.toDegrees (sp.getTheta (second))) 
+				+ "\t" +
+				nf.format (Math.toDegrees (rp.getLastTheta ())));
+			//	+ "\t" + 
+			//	nf.format (Math.toDegrees (rpCoarse.getLastTheta ())));
+	}
+	
+  }//ends main method
+}//ends class
 
